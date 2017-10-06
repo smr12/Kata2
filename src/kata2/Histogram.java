815f3eaaa5503106ebeda9d0df3d10d4bf531b8d
Entrar <Entrar@APD-L13>
2017-10-06 10:01:10 +0100

    Cuarta versión de Kata2

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -3,19 +3,19 @@ package kata2;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Histogram {
-    private final int [] data;
+public class Histogram <T> {
+    private final T [] data;
 
-    public Histogram(int[] data) {
+    public Histogram(T[] data) {
         this.data = data;
     }
 
-    public int[] getData() {
+    public T[] getData() {
         return data;
     }
-    public Map<Integer, Integer> getHistogram(){
-        Map<Integer, Integer> histogram = new HashMap<>();
-        for (int key : data) {
+    public Map<T, Integer> getHistogram(){
+        Map<T, Integer> histogram = new HashMap<>();
+        for (T key : data) {
             histogram.put(key, histogram.containsKey(key) ? 
                     histogram.get(key) + 1 : 1);
         }
--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -4,13 +4,12 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        int [] data = {1,1,1,0,3,4,4,3,0,0,1,1,1,1,3,2};
+        Integer [] data = {1,1,1,0,3,4,4,3,0,0,1,1,1,1,3,2};
         Histogram histo = new Histogram(data);
         Map<Integer, Integer> histogr = histo.getHistogram();
        
         for (int key : histogr.keySet()){
-            System.out.println("El número " + key + " aparece " 
-                    + histogr.get(key) + " veces");
+            System.out.println(key + " aparece " + histogr.get(key) + " veces");
         }    
     }
 }
