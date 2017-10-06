3ba44b5610385b772ce003f369603e7a835c4aeb
Entrar <Entrar@APD-L13>
2017-10-06 09:53:45 +0100

    Tercera versión de Kata2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,19 +1,16 @@
 package kata2;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Kata2 {
 
     public static void main(String[] args) {
         int [] data = {1,1,1,0,3,4,4,3,0,0,1,1,1,1,3,2};
-        Map<Integer, Integer> histogram = new HashMap<>();
-        for (int key : data) {
-            histogram.put(key, histogram.containsKey(key) ? 
-                    histogram.get(key) + 1 : 1);
-        }
-        for (int key : histogram.keySet()){
+        Histogram histo = new Histogram(data);
+        Map<Integer, Integer> histogr = histo.getHistogram();
+       
+        for (int key : histogr.keySet()){
             System.out.println("El número " + key + " aparece " 
-                    + histogram.get(key) + " veces");
+                    + histogr.get(key) + " veces");
         }    
     }
 }
